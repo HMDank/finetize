e8125846dbffd18a1311a7cadf1f07fb9bbd7958
Dank <dang.hoang@vucar.com>
2024-02-28 22:39:44 +0700

    Final touches

--- Finetize.py
+++ Finetize.py
@@ -5,10 +5,11 @@ st.set_page_config(layout="wide",
 
 
 st.title("Finetize", anchor=False)
-st.subheader('Simulate a Trading Strategy, only with 2 steps!', anchor=False)
+st.subheader('Simulate a Trading Strategy, in 3 steps!', anchor=False)
 st.markdown('''
 1. Navigate through the sidebar and select a symbol (and period away from today) in `Analysis` to see the symbol's analysis.
 2. Head to `Simulation` to simulate a trading strategy on that same exact stock data!
+3. Test the Strategy in `Test` and see how it holds against the market!
 ''')
 st.write('')
 st.caption("(Please feel free to contact me via the link on the sidebar!)")
